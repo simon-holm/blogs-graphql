493eb762fa4dabfcd9312d135cce01603a7707b9
spixooze <simon.code@outlook.com>
2018-06-06 14:36:19 +0200

    dev / prod - config

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,5 @@
-const Config = {
-  SESSION_SECRET: process.env.SESSION_SECRET || '1928kojdoiu3rlkjd',
-  DB_URI: process.env.DB_URI || 'mongodb://spixooze:abc123@ds219040.mlab.com:19040/blogs-graphql'
-}
-
-module.exports = {
-  ...Config
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./prod')
+} else {
+  module.exports = require('./dev')
 }
---
+++ src/config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  SESSION_SECRET: process.env.SESSION_SECRET,
+  DB_URI: process.env.DB_URI
+}
